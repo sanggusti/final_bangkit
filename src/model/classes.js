51256bd17e17c13b5546a21d7066a4824acc7a25
Gusti Triandi Winata <gustiowinata0@gmail.com>
2020-06-21 13:28:35 +0700

    Ariefhirmanto branch merging to master (#5)
    * initialize webapp
    
    * try implement result with reaction text
    
    * change model to christina model
    
    * add favicon and title
    
    Co-authored-by: Arief Hirmanto <ariefhirmanto7@gmail.com>

---
+++ src/model/classes.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// Theses classes should correspond to the softmax output of your model.
+
+export const MODEL_CLASSES = {
+  0: 'Fresh Apple',
+  1: 'Fresh Banana',
+  2: 'Fresh Orange',
+  3: 'Rotten Apple',
+  4: 'Rotten Banana',
+  5: 'Rotten Orange'
+};
